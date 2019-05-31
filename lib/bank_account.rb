b32906287ff9a201e8abb1ee5eabbe84a282df96
Alethea Cary <aletheacary@gmail.com>
2019-05-31 02:28:51 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
-  attr_accessor :name, :variable_n, :balance
+  attr_accessor :name, :variable_n, :balance, :status
 
   def initialize (name)
     variable_n = self
     @name = name
     @balance = 1000
+    @status = "open"
   end
 
 end
