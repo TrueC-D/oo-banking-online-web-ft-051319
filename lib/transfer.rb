fc0968ffd6eee4a109782f8852ffaf3c5d912c4d
Alethea Cary <aletheacary@gmail.com>
2019-05-31 02:38:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :reciever, :amount
+  
+  def initialize (sender, reciever, amount=0)
+    @sender = sender
+    @reciever = reciever
+    @amount = amount
+    
+  end
+  
+  
 end
